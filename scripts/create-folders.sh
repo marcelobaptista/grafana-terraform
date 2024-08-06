6eeed9cdc86e4c2f6a80c27ef45de8bb6663b57e
Marcelo Baptista <marcelobaptista@outlook.com>
2024-08-05 22:16:08 -0300

    changes

--- scripts/create-folders.sh
+++ scripts/create-folders.sh
@@ -1,131 +1,130 @@
 #!/bin/bash
 
+# Variáveis para URL e Token do Grafana
 grafana_url=
+grafana_token=
 org_id=
 org_name=
-token_key=
 
-cat <<EOF >>"./${org_name}/import-${org_name}-resources.sh"
+# Obtém a versão mais recente do Terraform Provider para Grafana
+grafana_tf_version=$(
+  curl -s "https://api.github.com/repos/grafana/terraform-provider-grafana/releases/latest" |
+    jq -r '.tag_name | sub("^v"; "")'
+)
+
+# Obtém os UIDs das pastas do Grafana
+curl -ksH "Authorization: Bearer ${grafana_token}" \
+  "${grafana_url}/api/folders" |
+  jq -r '.[].uid' >folderUIDs
+
+# Verifica se os UIDs foram obtidos corretamente
+if [[ ! -s folderUIDs || $(<folderUIDs) == "null" ]]; then
+  echo "Verifique a URL e Token"
+  exit 1
+fi
+
+# Cria script de importação
+cat <<EOF >"./${org_name}/import-${org_name}-resources.sh"
 #!/bin/bash
 
 terraform init
 EOF
 
+# Cria arquivo principal do Terraform
 cat <<EOF >"./${org_name}/modules/folders/main.tf"
 terraform {
   required_providers {
     grafana = {
-      source = "grafana/grafana"
-      version = ">= 2.5.0"
+      source  = "grafana/grafana"
+      version = ">= ${grafana_tf_version}"
     }
   }
 }
 EOF
 
-chmod +x "./${org_name}/import-${org_name}-resources.sh"
-
-curl -ksH "Authorization: Bearer ${token_key}" "${grafana_url}/api/folders" |
-  jq -r '.[].uid' >folderUIDs
-
+#
 while IFS= read -r folderUid; do
-  curl -ksH "Authorization: Bearer ${token_key}" "${grafana_url}/api/folders/${folderUid}/permissions" |
-    jq -r >temp.json
-  slug=$(
-    jq -r '.[0].title' temp.json |
-      sed -e 's/ - / /g' -e 's/ /-/g' -e 's/\(.*\)/\L\1/' |
-      tr -d '"' | tr -s '-' |
-      sed -e 's/&/e/g' -e 's/@/a/g' -e 's/\///g'
-  )
-  folder=$(jq -cr '.[0].title' temp.json)
+  # Obtém permissões da pasta
+  curl -ksH "Authorization: Bearer ${grafana_token}" \
+    "${grafana_url}/api/folders/${folderUid}/permissions" |
+    jq -r >./folder.json
 
+  # Extrai slug e título da pasta
+  slug=$(jq -cr '.[0].slug' ./folder.json)
+  title=$(jq -cr '.[0].title' ./folder.json)
+
+  # Cria arquivo Terraform para a pasta
   cat <<EOF >>"./${org_name}/modules/folders/${slug}.tf"
 resource "grafana_folder" "${slug}" {
-  title                        = "${folder}"
+  title                        = "${title}"
   uid                          = "${folderUid}"
   org_id                       = "${org_id}"
   prevent_destroy_if_not_empty = true
 }
-
 EOF
 
-  team_permissions=$(
-    jq -r '[.[] | select(.teamId!=0) | {teamName: .team, team: .teamId,permission: .permissionName}]' temp.json
-  )
-  user_permissions=$(
-    jq -r '[.[] | select(.userId!=0) | {userLogin: .userLogin, userId: .userId, permission: .permissionName}]' temp.json
-  )
+  #
+  jq -r '
+  [.[] | 
+  select((.teamId!=0 and .userId==0) 
+  or (.teamId==0 and .userId!=0))]' ./folder.json >./permissions.json
 
-  if [ "${team_permissions}" == "[]" ] && [ "${user_permissions}" == "[]" ]; then
-    cat <<EOF >>"./${org_name}/modules/folders/${slug}.tf"
+  # Obtém o tamanho do arquivo JSON
+  length=$(jq '. | length' ./permissions.json)
+
+  # Adiciona permissões padrão ao arquivo Terraform
+  cat <<EOF >>"./${org_name}/modules/folders/${slug}.tf"
 resource "grafana_folder_permission" "${slug}_permissions" {
   depends_on = [grafana_folder.${slug}]
   folder_uid = grafana_folder.${slug}.uid
-  org_id     = "${org_id}"
-
   permissions {
     role       = "Viewer"
     permission = "View"
   }
-
   permissions {
     role       = "Editor"
     permission = "Edit"
   }
-}
-EOF
-  else
-
-    length=$(jq '. | length' temp.json)
-
-    cat <<EOF >>"./${org_name}/modules/folders/${slug}.tf"
-resource "grafana_folder_permission" "${slug}_permissions_users" {
-  depends_on = [grafana_folder.${slug}]
-  folder_uid = grafana_folder.${slug}.uid
-  org_id     = "${org_id}"
-
-  permissions {
-    role       = "Viewer"
-    permission = "View"
-  }
-
-  permissions {
-    role       = "Editor"
-    permission = "Edit"
-  }
-
 EOF
 
-    for ((i = 0; i < length; i++)); do
-      teamId=$(jq -r ".[$i].teamId" temp.json)
-      teamName=$(jq -r ".[$i].team" temp.json)
-      userId=$(jq -r ".[$i].userId" temp.json)
-      userLogin=$(jq -r ".[$i].userLogin" temp.json)
-      permission=$(jq -r ".[$i].permissionName" temp.json)
+  # Adiciona permissões personalizadas ao arquivo Terraform
+  for ((i = 0; i < length; i++)); do
+    #
+    permissionName=$(jq -r ".[${i}].permissionName" ./folder.json)
+    teamId=$(jq -r ".[${i}].teamId" ./folder.json)
+    teamName=$(jq -r ".[${i}].team" ./folder.json)
+    userId=$(jq -r ".[${i}].userId" ./folder.json)
+    userLogin=$(jq -r ".[${i}].userLogin" ./folder.json)
 
-      if [ "$teamId" != "0" ]; then
-        cat <<EOF >>"./${org_name}/modules/folders/${slug}.tf"
-  permissions {
-    team_id    = "${teamId}" # ${teamName}
-    permission = "${permission}"
-  }
-
-EOF
-      fi
-      if [ "$userId" != "0" ]; then
-        cat <<EOF >>"./${org_name}/modules/folders/${slug}.tf"
+    if [ "${teamId}" == "0" ]; then
+      cat <<EOF >>"./${org_name}/modules/folders/${slug}.tf"
   permissions {
     user_id    = "${userId}" # ${userLogin}
-    permission = "${permission}"
+    permission = "${permissionName}"
   }
-
 EOF
-      fi
-    done
-    echo "}" >>"./${org_name}/modules/folders/${slug}.tf"
-  fi
+    else
+      cat <<EOF >>"./${org_name}/modules/folders/${slug}.tf"
+  permissions {
+    team_id    = "${teamId}" # ${teamName}
+    permission = "${permissionName}"
+  }
+EOF
+    fi
+  done
 
+  # Fecha arquivo Terraform
+  echo "}" >>"./${org_name}/modules/folders/${slug}.tf"
+
+  # Adiciona comando de importação ao script de importação
   echo "terraform import module.folders.grafana_folder.${slug} ${folderUid}" >>"./${org_name}/import-${org_name}-resources.sh"
 
+  # Cleanup
+  rm -f ./folder.json ./permissions.json
 done <folderUIDs
 
-rm -rf "./${org_name}/modules/folders/*.json" folderUIDs
+# Adiciona permissões de execução ao script de importação
+chmod +x "./${org_name}/import-${org_name}-resources.sh"
+
+# Cleanup
+rm ./folderUIDs
--- scripts/create-tf-files.sh
+++ scripts/create-tf-files.sh
@@ -3,15 +3,21 @@
 admin_password=
 admin_user=
 grafana_url=
+grafana_token=
 org_name=
-token_key=
 
-cat <<EOF >"./${org_name}/providers.tf"
+# Obtém a versão mais recente do Terraform Provider para Grafana
+grafana_tf_version=$(
+  curl -s "https://api.github.com/repos/grafana/terraform-provider-grafana/releases/latest" |
+    jq -r '.tag_name | sub("^v"; "")'
+)
+
+cat <<EOF >"./${org_name}/main.tf"
 terraform {
   required_providers {
     grafana = {
       source                = "grafana/grafana"
-      version               = ">= 2.5.0"
+      version               = ">= ${grafana_tf_version}"
       configuration_aliases = [grafana.basic, grafana.token]
     }
   }
@@ -26,7 +32,7 @@ provider "grafana" {
 provider "grafana" {
   alias = "token"
   url   = "${grafana_url}"
-  auth  = "${token_key}"
+  auth  = "${grafana_token}"
 }
 EOF
 
